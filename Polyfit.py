fb478f6a6e8da16d918d8c4a14dcddf45eb08b04
Rana Khalil <rana.ae.khalil@gmail.com>
2017-02-19 03:17:49 -0800

    Add measureing curvature

--- Polyfit.py
+++ Polyfit.py
@@ -76,3 +76,49 @@ class Polyfit:
 
         return self.left_fit, self.right_fit
 
+    def curvature(self, img):
+        # Generate some fake data to represent lane-line pixels
+        ploty = np.linspace(0, 719, num=720)  # to cover same y-range as image
+        quadratic_coeff = 3e-4  # arbitrary quadratic coefficient
+        # For each y position generate random x position within +/-50 pix
+        # of the line base position in each case (x=200 for left, and x=900 for right)
+        leftx = np.array([200 + (y ** 2) * quadratic_coeff + np.random.randint(-50, high=51)
+                          for y in ploty])
+        rightx = np.array([900 + (y ** 2) * quadratic_coeff + np.random.randint(-50, high=51)
+                           for y in ploty])
+
+        leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
+        rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+
+        # Fit a second order polynomial to pixel positions in each fake lane line
+
+        # left_fit = np.polyfit(ploty, leftx, 2)
+        # left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+        # right_fit = np.polyfit(ploty, rightx, 2)
+        # right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+        ym_per_pix = 30 / 720  # meters per pixel in y dimension
+        xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+
+        # Define y-value where we want radius of curvature
+        # I'll choose the maximum y-value, corresponding to the bottom of the image
+        y_eval = np.max(ploty)
+        left_curverad = ((1 + (2 * self.left_fit[0] * y_eval + self.left_fit[1]) ** 2) ** 1.5) / np.absolute(2 * self.left_fit[0])
+        right_curverad = ((1 + (2 * self.right_fit[0] * y_eval + self.right_fit[1]) ** 2) ** 1.5) / np.absolute(2 * self.right_fit[0])
+
+        left_fit_cr = np.polyfit(ploty * ym_per_pix, leftx * xm_per_pix, 2)
+        right_fit_cr = np.polyfit(ploty * ym_per_pix, rightx * xm_per_pix, 2)
+        left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(2 * left_fit_cr[0])
+        right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(2 * right_fit_cr[0])
+        print(left_curverad, 'm', right_curverad, 'm')
+
+        ratio = left_curverad / right_curverad
+        print('Ratio: ', ratio)
+
+        lane_leftx = self.left_fit[0] * (img.shape[0] - 1) ** 2 + self.left_fit[1] * (img.shape[0] - 1) + self.left_fit[2]
+        lane_rightx = self.right_fit[0] * (img.shape[0] - 1) ** 2 + self.right_fit[1] * (img.shape[0] - 1) + self.right_fit[2]
+
+        car_pos = ((img.shape[1] / 2) - ((lane_leftx + lane_rightx) / 2)) * xm_per_pix
+
+        return (left_curverad + right_curverad) / 2, car_pos.round(2)
+
--- main.py
+++ main.py
@@ -31,11 +31,11 @@ warp = Warp(src, dst)
 undistort.calibratie_camera()
 
 def main():
-    # test_images = ['straight_lines1.jpg', 'straight_lines2.jpg', 'test1.jpg',
-    #                'test2.jpg', 'test3.jpg', 'test4.jpg', 'test5.jpg', 'test6.jpg']
-    # for image in test_images:
-    #     img = mpimg.imread('./test_images/' + image)
-    #     process_image(img, image)
+    test_images = ['straight_lines1.jpg', 'straight_lines2.jpg', 'test1.jpg',
+                   'test2.jpg', 'test3.jpg', 'test4.jpg', 'test5.jpg', 'test6.jpg']
+    for image in test_images:
+        img = mpimg.imread('./test_images/' + image)
+        process_image(img, image)
     white_output = 'project_video_done_2.mp4'
     clip1 = VideoFileClip('project_video.mp4')
     white_clip = clip1.fl_image(process_image)  # NOTE: this function expects color images!!
@@ -51,6 +51,19 @@ def process_image(base_image):
     img = warp.warp(img)
     left_fit, right_fit = polyfit.polyfit(img, visualize=False)
     img = polydraw.draw(img_undist, left_fit, right_fit, warp.Minv)
+    # Measure curvature
+    lane_curve, car_position = polyfit.curvature(img)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    if car_position > 0:
+        cv2.putText(img, 'Radius of curvature (Left)  = %.2f m' % (lane_curve), (10, 40), font, 1,
+                    (255, 255, 255), 2, cv2.LINE_AA)
+    else:
+        cv2.putText(img, 'Radius of curvature (Right) = %.2f m' % (lane_curve), (10, 70), font, 1,
+                    (255, 255, 255), 2, cv2.LINE_AA)
+
+    cv2.putText(img, "Lane curve: {}m".format(lane_curve.round()), (10, 50), font, 1,
+                color=(255, 255, 255), thickness=3)
     return img
 
 if __name__ == '__main__':
